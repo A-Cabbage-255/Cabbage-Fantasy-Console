d10a112037362b050989b626081d85f69ad1dc3b
Caleb Blake <calebb564738@gmail.com>
2026-04-13 21:07:16 -0700

    Two-Pass reading of file, constructing labels/pointers

--- src/asm/asmparser.h
+++ src/asm/asmparser.h
@@ -1,16 +1,20 @@
 #pragma once
 #include "../common.h"
 #include "SDL3/SDL.h"
+#include <map>
 
+//TODO COMPLETELY REFACTOR WITH TOKEN PARSING ETC.
 class Assembler {
 private:
-	int pass = 0;
-
-	SDL_IOStream* file = nullptr;
+	SDL_IOStream* reading = nullptr;
 
 	std::string readLine();
 public:
 	bool eof = false;
+	int pass = 0;
+	unsigned line = 0;
+	
+	std::map<std::string, Uint32> pointers;
 
 	Assembler(std::string filePath);
 	~Assembler();
--- src/asm/main.cpp
+++ src/asm/main.cpp
@@ -3,9 +3,21 @@
 #include "asmparser.h"
 
 int main(int argc, char* argv[]) {
-	Assembler a("");
+	Assembler a("assets/asm.asm");
 
-	a.parseLine();
+	std::cout << "\tSTART\n";
+
+	while (!a.eof) {
+		a.parseLine();
+	}
+
+	std::cout << "\tEND\n";
+
+	std::cout << "Pointers:\n";
+
+	for (auto it = a.pointers.cbegin(); it != a.pointers.cend(); ++it) {
+		std::cout << "\t{" << it->first << ", " << it->second << "}\n";
+	}
 
 	return 0;
 }
